e5543fb1e359238295ffbb595c12c3c553dbc10d
Klaudia Antczak <klaantczak@gmail.com>
2017-05-16 14:18:50 +0200

    rails task app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :tasks
+#read all tasks
+#get "tasks", to: "tasks#index"
+
+#read one specific task
+#get "tasks/:id", to: "tasks#show"
+
+#create a new task
+#get "tasks/new", to: "tasks#new"
+#post "tasks", to: "tasks#create"
+
+#update a task
+#get "tasks/:id/edit", to: "tasks#edit"
+#patch "tasks/:id", to: "tasks#update"
+
+#delete a task
+#delete "tasks/:id", to: "tasks#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(name: "YOU", description: ";D")
+Task.create(name: "Go to the gym", description: "do some yoga")
+Task.create(name: "Cook dinner", description: "At 7 pm")
